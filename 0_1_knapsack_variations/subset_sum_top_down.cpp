4441c0fca83b7de50812492eb994e82fe88d0b69
AlAminMiah4274 <alaminmiah4274@gmail.com>
2024-10-04 23:36:28 +0600

    discussed about subset sum and implemented subset sum using top down approach

---
+++ 0_1_knapsack_variations/subset_sum_top_down.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int subset_sum_optimization[1005][1005];
+bool subset_sum(int n, int arr[], int sum)
+{
+    if (n == 0)
+    {
+        if (sum == 0)
+            return true;
+        else
+            return false;
+    }
+
+
+    if (subset_sum_optimization[n][sum] != -1) return subset_sum_optimization[n][sum];
+
+    if (arr[n - 1] <= sum)
+    {
+        int chosen = subset_sum(n - 1, arr, sum - arr[n - 1]);
+        int not_chosen = subset_sum(n - 1, arr, sum);
+
+        return subset_sum_optimization[n][sum] = chosen || not_chosen;
+    }
+    else
+    {
+        int not_chosen = subset_sum(n - 1, arr, sum);
+        return subset_sum_optimization[n][sum] = not_chosen;
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int sum_arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> sum_arr[i];
+    }
+
+    int sum;
+    cin >> sum;
+
+    memset(subset_sum_optimization, -1, sizeof(subset_sum_optimization));
+
+    if (subset_sum(n, sum_arr, sum))
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+
+    return 0;
+}
